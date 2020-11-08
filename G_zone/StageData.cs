47494741f26bea9252cb9d46238f891af6fb68b6
KitchenGun <kitchengun124@gmail.com>
2020-11-08 19:43:34 +0900

    파일

---
+++ G_zone/StageData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageData : MonoBehaviour {
+
+    static public int score = 0;
+    static public int time = 0;
+    static public int touchNumber = 0;
+    static public int distance = 0;
+
+}
